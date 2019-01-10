29ae913670d872d26b00fb9f7757272d543ea779
JevinJ <jevinjones@openmailbox.org>
2019-01-10 00:44:16 -0800

    Rework by_tag_a() and by_xpath(). Remove unnecessary prints, cleaning up.

--- get_links.py
+++ get_links.py
@@ -3,36 +3,30 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 def by_tag_a(selenium_driver, logging_context=logging.getLogger('GetLinkLogger')):
-    logging_context.info('Getting links by tag')
+    logging_context.info('Getting links by tag...')
     links = []
     try:
-        logging_context.info('Finding link elements')
-        print('Finding link elements')
+        logging_context.info('Extracting links...')
         elements = selenium_driver.find_elements_by_tag_name('a')
-        print('Extracting links')
-        links += ([element.get_attribute('href') for element in elements if element.get_attribute('href') != None])
+        links.extend([element.get_attribute('href') for element in elements if element.get_attribute('href') != None])
         logging_context.debug('Links found : ' + str(links))
         return links
     except NoSuchElementException:
         logging_context.warning('NoSuchElementException finding job description links')
-        print('NoSuchElementException')
 
 
 def by_xpath(selenium_driver, selector, logging_context=logging.getLogger('GetLinkLogger')):
-    logging_context.info('Getting links by xpath')
+    logging_context.info('Getting links by xpath...')
     links = []
     for index in range(1001):
+        formatted_selector = selector.format(index)
         try:
-            elements = selenium_driver.find_elements_by_xpath(selector.format(index))
-            logging_context.info('Found elements by xpath: ' + selector.format(str(index)))
+            logging_context.info('Extracting links...')
+            elements = selenium_driver.find_elements_by_xpath(formatted_selector)
             links += [element.get_attribute('href') for element in elements]
-        except NoSuchElementException:
-            logging_context.warning(
-                'NoSuchElementException getting element by xpath: ' + selector.format(str(index)))
-    if links:
-        logging_context.info('Returning links: ' + str(links))
-    else:
-        logging_context.info('No links found')
+        except NoSuchElementException as exc:
+            logging_context.exception(msg='{exc}: {selector}'.format(exc=exc, selector=formatted_selector))
+    logging_context.debug('Links found : ' + str(links))
     return links
 
 
