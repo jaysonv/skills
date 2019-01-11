f623dd4011c4289abf4ccb074c65d25b562b638b
JevinJ <jevinjones@openmailbox.org>
2019-01-10 20:10:06 -0800

    Simplified 'INDEED_URL'

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ KEY_WORDS = frozenset(PROGRAM_LANGUAGES + ANALYSIS_SOFTWARE + BIGDATA_TOOL + DAT
 JOB_DESCRIPTION_IDENTIFIER = 'clk?jk'
 
 INDEED_JOB_DESCRIPTION_TITLE_SELECTOR = '/html/body/div[1]/div[3]/div[3]/div/div/div[1]/div[1]/div[1]/h3'
-INDEED_URL = 'https://www.indeed.com/jobs?as_and=software+quality+assurance+engineer&as_any=&as_not=&as_ttl=&as_cmp=&jt=fulltime&st=&as_src=&salary=%24145%2C000%2B&radius=50&l=95032&fromage=60&limit=50&sort=&psf=advsrch'
+INDEED_URL = 'https://www.indeed.com/jobs?q=software+quality+assurance+engineer+%24145%2C000%2B&l=95032'
 INDEED_PAGING_SELECTOR = '//*[@id="resultsCol"]/div[28]/a[{}]'
 INDEED_JOB_LINK_SELECTOR_TYPE = 'tag'
 INDEED_JOB_LINK_SELECTOR = None
