238855b73b4a92e17a63fa3f3a867f530a1dcb47
JevinJ <jevinjones@openmailbox.org>
2019-01-11 20:25:08 -0800

    Add 'next_page_selector_type' parameter. Add/modify link finder functions. Rename get_job_post_links() to get_all_job_post_links(). Rename 'self.job_posting_title_selector' to 'self.job_link_title_selector'

--- src/job_site.py
+++ src/job_site.py
@@ -1,8 +1,7 @@
-from get_job_links import get_link_func
 from job_description import JobDescription
 import logging
 from selenium.common.exceptions import NoSuchElementException
-from utility import log_result
+from utility import log_result, get_link_finder_func
 
 
 JOB_DESCRIPTION_IDENTIFIER = 'clk?jk'
@@ -11,17 +10,22 @@ JOB_OUTPUT_FILENAME = '../job_output.txt'
 
 
 class JobSite(object):
-    def __init__(self, selenium_driver, url, next_page_selector, job_link_selector_type, job_link_selector,
+    def __init__(self, selenium_driver, url, next_page_selector, next_page_selector_type, job_link_selector_type, job_link_selector,
                  job_posting_title_selector):
         self.selenium_driver = selenium_driver
         self.url = url
+        self.current_page = 1
         self.discarded_job_descriptions = set()
         self.job_postings = []
+
         self.next_page_selector = next_page_selector
-        self.job_posting_title_selector = job_posting_title_selector
-        self.get_job_links = get_link_func(
+        self.job_link_title_selector = job_posting_title_selector
+        self.job_link_selector = job_link_selector
+
+        self.get_job_links_on_page = get_link_finder_func(
             job_link_selector_type, selenium_driver, job_link_selector)
-        self.current_page = 1
+        self.get_next_page_link = get_link_finder_func(
+            next_page_selector_type, selenium_driver, next_page_selector, single_link=True)
 
     def go_to_start_page(self):
         self.selenium_driver.get(self.url)
@@ -60,7 +64,7 @@ class JobSite(object):
             logging.info('Writing results to: {output_filename}'.format(output_filename=JOB_OUTPUT_FILENAME))
 
     @log_result
-    def get_job_post_links(self, solitary_pages=False):
+    def get_all_job_post_links(self, solitary_pages=False):
         """
         Walk through all pages of a site and collect all job posting links.
         :param solitary_pages: If True, results will be collected on a per-page basis.
@@ -76,17 +80,17 @@ class JobSite(object):
             print('Looking on page {}...'.format(self.current_page))
             logger.info('Looking on page {}...'.format(self.current_page))
             if solitary_pages:
-                job_posting_links.extend(self.get_job_links())
+                job_posting_links.extend(self.get_job_links_on_page())
             if not self.go_to_next_page():
                 break
         if not solitary_pages:
-            job_posting_links.extend(self.get_job_links())
+            job_posting_links.extend(self.get_job_links_on_page())
         return job_posting_links
 
     def process_site(self):
-        links = self.get_job_post_links()
+        links = self.get_all_job_post_links(solitary_pages=True)
         for link in links:
             self.job_postings.append(
-                JobDescription.from_url(link, self.selenium_driver, self.job_posting_title_selector))
+                JobDescription.from_url(link, self.selenium_driver, self.job_link_title_selector))
         self.discard_unmatched_job_descriptions()
         self.output_results()
