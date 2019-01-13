9f3f29fb3829d0a79bab5d5d27e01cf0535d5eaa
JevinJ <jevinjones@openmailbox.org>
2019-01-12 18:22:36 -0800

    Add multiprocessing.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import logging
+from functools import partial
 from job_posting_link_crawler import JobPostingLinkCrawler
 from job_posting_parser import JobPostingParser
+import multiprocessing
 from selenium import webdriver
 import signal
 from utility import make_date_string
@@ -50,38 +52,78 @@ SITE_DICT = {
             },
 '''
 
-driver = webdriver.Firefox(service_log_path='../logs/geckodriver.log')
+
+def cleanup(driver):
+    driver.quit()
+
+
+def crawl_site_for_job_links(site_config):
+    driver = webdriver.Firefox()
+    signal.signal(signal.SIGTERM, partial(cleanup, driver))
+    signal.signal(signal.SIGINT, partial(cleanup, driver))
+    try:
+        crawler = JobPostingLinkCrawler(driver, site_config)
+        crawler.start()
+    except Exception:
+        raise
+    finally:
+        cleanup(driver)
+    return crawler.found_links
+
+
+def parse_job_posting_links(*args):
+    posting_links, site_config = args
+
+    driver = webdriver.Firefox()
+    signal.signal(signal.SIGTERM, partial(cleanup, driver))
+    signal.signal(signal.SIGINT, partial(cleanup, driver))
+    try:
+        parser = JobPostingParser(driver, posting_links, site_config)
+        parser.start()
+    except Exception:
+        raise
+    finally:
+        cleanup(driver)
+    return parser.job_postings
+
+
 
 def main():
     sites = {'indeed': {
-        'search_start_url': 'https://www.indeed.com/jobs?q=software+quality&l=95032',
-        'next_page_selector': '//span[.={}]/..',
-        'next_page_selector_type': 'xpath',
-        'job_link_selector': 'a[class="jobtitle turnstileLink"]',
-        'job_link_selector_type': 'css',
-        'job_posting_title_selector': 'h3[class$="JobInfoHeader-title"]',
-        'job_posting_title_selector_type': 'css',
-        'job_posting_description_selector': 'div[class^="jobsearch-JobComponent-description"]',
-        'job_posting_description_selector_type': 'css',
-        'use_solitary_paging': True}
-    }
+            'search_start_url': 'https://www.indeed.com/jobs?q=software+quality&l=95032',
+            'next_page_selector': '//span[.={}]/..',
+            'next_page_selector_type': 'xpath',
+            'job_link_selector': 'a[class="jobtitle turnstileLink"]',
+            'job_link_selector_type': 'css',
+            'job_posting_title_selector': 'h3[class$="JobInfoHeader-title"]',
+            'job_posting_title_selector_type': 'css',
+            'job_posting_description_selector': 'div[class^="jobsearch-JobComponent-description"]',
+            'job_posting_description_selector_type': 'css',
+            'use_solitary_paging': True},
+        'careerbuilder': {
+            'search_start_url': 'https://www.indeed.com/jobs?q=software+quality&l=95032',
+            'next_page_selector': '//span[.={}]/..',
+            'next_page_selector_type': 'xpath',
+            'job_link_selector': 'a[class="jobtitle turnstileLink"]',
+            'job_link_selector_type': 'css',
+            'job_posting_title_selector': 'h3[class$="JobInfoHeader-title"]',
+            'job_posting_title_selector_type': 'css',
+            'job_posting_description_selector': 'div[class^="jobsearch-JobComponent-description"]',
+            'job_posting_description_selector_type': 'css',
+            'use_solitary_paging': True}
+        }
 
-    logging.info('PROCESSING INDEED')
-    print('PROCESSING INDEED')
-    indeed_links = JobPostingLinkCrawler(driver, **sites['indeed']).start()
-    print(indeed_links)
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    job_posting_links = pool.map(crawl_site_for_job_links, sites.values())
+    job_postings = pool.starmap(parse_job_posting_links, zip(job_posting_links, sites.values()))
+    job_postings = [item for sublist in job_postings for item in sublist]
+    for job in job_postings:
+        print(job)
 
 
 if __name__ == '__main__':
-    def cleanup(*args):
-        driver.quit()
-
-    signal.signal(signal.SIGTERM, cleanup)
-    signal.signal(signal.SIGINT, cleanup)
     try:
         main()
     except Exception as exc:
         logging.exception(exc)
         raise
-    finally:
-        cleanup()
