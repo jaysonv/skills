de3295f1ab520368947a02f287fdbd77ff185446
JevinJ <jevinjones@openmailbox.org>
2019-01-12 18:41:33 -0800

    Remove SITE_DICT

--- src/main.py
+++ src/main.py
@@ -8,49 +8,6 @@ import signal
 from utility import make_date_string
 
 
-'''
-SITE_DICT = {
-    'dice':
-        {
-            'url': 'https://www.dice.com/jobs/advancedResult.html?for_one=&for_all={title}&for_exact=&for_none=&for_jt=&for_com=&for_loc=Santa+Clara%2C+CA&jtype=Full+Time&sort=relevance&limit=100&radius=50&jtype=Full+Time&limit=100&radius=50&jtype=Full+Time',
-            'format_string': None,
-            'job_link_element_selector_type': None,
-
-            'job_element_selector': None,
-            'next_page_selector': '/html/body/div[8]/div[3]/div[2]/div[1]/div[6]/div[2]/div/ul/li[{}]/a',
-            'paging_type': 'page',
-            },
-    'monster':
-        {
-            'url': 'https://www.monster.com/jobs/search/Full-Time_8?q=software-quality-assurance&intcid=skr_navigation_nhpso_searchMain&rad=50&where=Los-Gatos__2c-CA&tm=30',
-            'format_string': None,
-            'job_link_element_selector_type': None,  # NEED TO BE "PAGED    "
-            'job_element_selector': None,
-            'next_page_selector': '//*[@id="loadMoreJobs"]',
-            'paging_type': 'button',
-
-        },
-    'glass_door':
-        {
-            'url': 'https://www.glassdoor.com/Job/jobs.htm?sc.keyword=Software%20Quality%20Assurance%20Engineer&locT=C&locId=1147436&locKeyword=San%20Jose,%20CA&jobType=fulltime&fromAge=30&minSalary=170000&includeNoSalaryJobs=false&radius=25&cityId=-1&minRating=4.00&industryId=-1&companyId=-1&applicationType=0&employerSizes=0&remoteWorkType=0',
-            'format_string': None,
-            'job_link_element_selector_type': None,
-            'job_element_selector': None,
-            'next_page_selector': '/html/body/div[3]/div/div/div/div[1]/div/div[2]/section/article/div/div[3]/div[2]/div/div/ul/li[{}]/a',
-            'paging_type': 'page',
-            },
-    'linked_in':
-        {
-            'url': 'https://www.linkedin.com/jobs/search/?distance=50&f_E=3%2C4&f_JT=F&f_SB2=5&f_TP=1%2C2%2C3%2C4&keywords=software%20quality%20assurance%20engineer&location=Santa%20Clara%2C%20California&locationId=PLACES.us.7-1-0-43-18',
-            'format_string': None,
-            'job_link_element_selector_type': None,
-            'job_element_selector': None,
-            'next_page_selector': '//*[@id="ember975"]/li/ol/li[{}]/button',
-            'paging_type': 'page',  # yes, page!
-            },
-'''
-
-
 def cleanup(driver):
     driver.quit()
 
